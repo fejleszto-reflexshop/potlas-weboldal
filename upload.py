8600d6185b2b7a5130dcb2fb23cfac91795f4b8b
Fejleszto Reflexshop <fejleszto@reflexshop.hu>
2026-06-26 09:55:39 +0200

    refactor: date updated for commit string

--- upload.py
+++ upload.py
@@ -16,7 +16,7 @@ def run_git(args):
 
 def main():
     p_add = run_git(["add", "."])
-    p_commit = run_git(["commit", "-m", f"deploy: new games added at {datetime.now().strftime('YYYY:mm:dd')}"])
+    p_commit = run_git(["commit", "-m", f"deploy: new games added at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"])
     p_push = run_git(["push"])
 
     with open(f"{PROJECT_DIR}\\log_upload.log", "a", encoding="utf-8") as file:
